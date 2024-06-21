f16a205f500b5b6d75b9a313db19ba2650e653ab
ShrutiDeshmane12 <shrutideshmane123@gmail.com>
2024-06-21 15:23:55 +0530

    first commit

---
+++ src/main/java/com/example/JenkinDemo/JenkinDemoApplication.java
@@ -0,0 +1,21 @@
+package com.example.JenkinDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class JenkinDemoApplication {
+	
+	@GetMapping
+	public String getmessage()
+	{
+		return "Hello";
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinDemoApplication.class, args);
+	}
+
+}
